bf91ceeeb24070010ed43c7e013554b27f899ed9
rezkyyayang <rezky.yayang@gmail.com>
2025-06-12 11:50:35 +0700

    Update analytics

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
+  <Analytics />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
